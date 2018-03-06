29152fe7823421a11b18edb668661f9575eb1a44
Roland Adams <roland.adams@smartideasllc.net>
2018-03-05 22:06:55 -0500

    add logging and .ignore file

--- openkmclient.js
+++ openkmclient.js
@@ -1,5 +1,7 @@
 'use strict';
-var Client = require('node-rest-client').Client;
+
+var Client  = require('node-rest-client').Client;
+var logger = require('winston');
 
 module.exports = class openkmClient {
 
@@ -7,9 +9,27 @@ module.exports = class openkmClient {
        this.openkmHost = hostandport
        this.openkmUser = user
        this.openkmPwd = password
-       this.protocal = (isHttps === 'true') ? 'https://' : 'http://';
+
+       this.protocal = 'http://'
+       if(isHttps){
+         this.protocal = 'https://'
+       }
+
        var options_auth = { user: user, password: password };
+
+       var args = {
+         headers: { "Content-Type": "application/json","Accept":"application/json" }
+       }
+
        this.client = new Client(options_auth);
+
+       logger.configure({
+            transports: [
+              new (logger.transports.File)({ filename: 'openkmclient.log',level: 'debug' })
+            ]
+          });
+
+     logger.log('debug',isHttps)
    }
 
    addDocument(docName) {
@@ -20,8 +40,8 @@ module.exports = class openkmClient {
    var args = {
      headers: { "Content-Type": "application/json" }
    }
-   var url = "http://"+this.openkmHost+"/OpenKM/services/rest/document/createSimple"
-       console.log("openkmClient " + docName);
+   var url = this.protocal+this.openkmHost+"/OpenKM/services/rest/document/createSimple"
+       logger.log("openkmClient " + docName);
    }
 
    addFolder(folderName) {
@@ -29,49 +49,56 @@ module.exports = class openkmClient {
    //  $ curl -u okmAdmin:admin -H "Accept: application/json" \
    // -X POST -H "Content-Type: application/json" -d '/okm:root/newfolder' \
    // http://localhost:8080/OpenKM/services/rest/folder/createSimple
-       console.log("openkmClient " + folderName);
+       logger.log("openkmClient " + folderName);
    }
 
    getDocumentByName(docName) {
-       console.log("openkmClient " + docName);
+       this.logger.log("openkmClient " + docName);
    }
 
    getDocumentById(docId) {
    //   # get File
    // curl -u web:web \
    // http://159.89.185.142:8080/OpenKM/services/rest/document/getContent?docId=303944b5-5b35-4b7a-a44e-a74bffe54df3
-       console.log("openkmClient " + docId);
+       logger.log("openkmClient " + docId);
    }
 
    listFilesByFolderName(folderName) {
-       console.log("openkmClient " + folderName);
+       logger.log("openkmClient " + folderName);
    }
 
    listFilesByFolderId(folderId) {
+     logger.debug('listFilesByFolderId')
    //   # list files in folder
    // curl -u okmAdmin:admin -H "Accept: application/json" \
    // http://localhost:8080/OpenKM/services/rest/folder/getChildren?fldId=3492d662-b58e-417c-85b6-930ad0c6c3cf
-   var url = "https://"+this.openkmHost+"OpenKM/services/rest/folder/getChildren?fldId=cf0c25ff-14d7-4c3f-b96e-be313e9f6991"
-       console.log("openkmClient " + folderId);
-       console.log()
+    var url = this.protocal+this.openkmHost+"/OpenKM/services/rest/document/getChildren?fldId="+folderId
+       logger.info("openkmClient " + folderId);
+       this.client.get(url, this.args, function (data, response) {
+           // parsed response body as js object
+           logger.log('debug',data)
+           // raw response
+           console.log(response.statusCode);
+           logger.log('info','statuscode: '+response.statusCode)
+           return JSON.stringify(data);
+       });
    }
 
 
    test() {
-     console.log('Get list of files from folder')
-     var args = {
-       headers: { "Content-Type": "application/json","Accept":"application/json" }
-     }
+     logger.debug('Get list of files from folder')
 
-     console.log('args: '+ args)
+     logger.log('debug','args: '+ args)
   //   var url = "https://"+this.openkmHost+"/OpenKM/services/rest/folder/getChildren?fldId=9e2f1775-f41f-46fb-8222-e21efad9abba"
-     var url = "https://"+this.openkmHost+"/OpenKM/services/rest/document/getChildren?fldId=cf0c25ff-14d7-4c3f-b96e-be313e9f6991"
-     console.log(url)
-     this.client.get(url, args, function (data, response) {
+     var url = this.protocal+this.openkmHost+"/OpenKM/services/rest/document/getChildren?fldId=cf0c25ff-14d7-4c3f-b96e-be313e9f6991"
+     logger.log('debug',url)
+     this.client.get(url, this.args, function (data, response) {
          // parsed response body as js object
-         console.log(data);
+         logger.log('debug',data)
          // raw response
          console.log(response.statusCode);
+         logger.log('info','statuscode: '+response.statusCode)
+         return JSON.stringify(data);
      });
    }
 }
